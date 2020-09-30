5dfccbb3af7b572f58d0e32987ce83aa64261e8d
Artur <alphaunionxt@gmail.com>
2020-09-30 13:50:43 -0400

    hard coded data transfers to client side, with a CORS package work around --local only

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+// app.use(cors({
+//     origin: [`http://localhost:3000/creators, `],
+//     methods: "GET,POST",
+//     optionsSuccessStatus: 200
+// }))
+
 require('dotenv').config();
 
 // app.use(express.json());
@@ -8,23 +16,33 @@ app.use(express.urlencoded({
 }));
 
 //  * CORS allows for Local Devlopment
+// ! Work around without using CORS (again only local)
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Header", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
-})
+});
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url} ${new Date().toLocaleTimeString()}`);
+    next();
+});
 
 app.get('/creators', async (req, res) => {
     const creators = [{
-            name: '',
+            name: 'John',
             img: 'https://'
         },
         {
-            name: '',
+            name: 'Becky',
             img: 'https://'
         },
         {
-            name: '',
+            name: 'George',
+            img: 'https://'
+        },
+        {
+            name: 'Michael',
             img: 'https://'
         },
     ];
