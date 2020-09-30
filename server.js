39920f456522e08236992f831c5677a05f2c0c2e
Artur <alphaunionxt@gmail.com>
2020-09-30 13:33:39 -0400

    Mock data, Routes used base struture with outlined todo's

--- server.js
+++ server.js
@@ -2,9 +2,42 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+// app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
 
+//  * CORS allows for Local Devlopment
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Header", "Content-Type");
+    next();
+})
 
-app.get('/', (req, res) => res.send("Server Up!"));
+app.get('/creators', async (req, res) => {
+    const creators = [{
+            name: '',
+            img: 'https://'
+        },
+        {
+            name: '',
+            img: 'https://'
+        },
+        {
+            name: '',
+            img: 'https://'
+        },
+    ];
+    // TODO  GET from DB
+    res.send(creators);
+});
+
+app.post('/creators', async (req, res) => {
+    // TODO : Scrape Chanell
+    //  TODO : Add to DB
+    console.log(req.body);
+    res.send("Success");
+});
 
 
 const PORT = process.env.PORT || 4000;
